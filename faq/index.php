23696ef1b0fb898b20cc5d5d45a6c2d1e05a34d4
Hack Keele <32229425+HackKeele@users.noreply.github.com>
2024-10-09 17:47:30 +0100

    Rewrite some of the FAQ to be a little easier to understand

--- faq/index.php
+++ faq/index.php
@@ -7,10 +7,13 @@ $awp_browser_title = "FAQ";
 <h1>FAQ</h1>
 <div class="ArticleBoxContainer">
 	<div class="ArticleBox1 ArticleBoxBlack">
-		<h1 class="ArticleBoxHeader">What is a hackathon?</h1>
-		<p>Hackathons are a place where many passionate professionals or students in technology and business development actively participate by forming diverse teams, making their pitches, adding their expertise and working towards creating a solution.</p>
-		<p>It is one such opportunity where you make many new friends from diverse backgrounds whom you would have never met, and learn new things from mentors and teammates. Above all, hackathons help you to see a problem from multiple perspectives and arrive at a solution.</p>
+		<h1 class="ArticleBoxHeader">What is Hack Keele, anyway?</h1>
+		<p>Hack Keele is the main general computing society for Keele University. We're not all computer science students, and we're one of the few free societies at the University.</p>
 	</div>
-	
+	<div class="ArticleBox1 ArticleBoxBlack">
+		<h1 class="ArticleBoxHeader">What is a hackathon?</h1>
+		<p>Hackathons are events where many passionate professionals and/or students in technology participate by forming teams, making pitches, adding their expertise and working towards creating a solution to a given problem.</p>
+		<p>Hackathons help you to see a problem from multiple perspectives and arrive at a solution whilst forming new relationships along the way.</p>
+	</div>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/include/components/footer.php" ?>

8febc4c7b63bf6ad6f7e2bd4add400d9c1854487
Hack Keele <32229425+HackKeele@users.noreply.github.com>
2024-10-09 18:06:19 +0100

    Remove unneeded sentence from FAQ, change link hover colour

--- faq/index.php
+++ faq/index.php
@@ -13,7 +13,6 @@ $awp_browser_title = "FAQ";
 	<div class="ArticleBox1 ArticleBoxBlack">
 		<h1 class="ArticleBoxHeader">What is a hackathon?</h1>
 		<p>Hackathons are events where many passionate professionals and/or students in technology participate by forming teams, making pitches, adding their expertise and working towards creating a solution to a given problem.</p>
-		<p>Hackathons help you to see a problem from multiple perspectives and arrive at a solution whilst forming new relationships along the way.</p>
 	</div>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . "/include/components/footer.php" ?>
